f2edd804ceaced89abf348c30e711f8a742f5cc9
pospelovav <shnurokvtaige@yandex.ru>
2020-10-16 11:22:08 +0300

    Добавлен код с урока Java Two Lesson D

---
+++ src/ru/geekbrains/java_two/lesson_d/home/ChatServer.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.java_two.lesson_d.home;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server stared at port: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
